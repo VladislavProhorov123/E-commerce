9ff25e3b06abe1a7f25c97c5dab462c3f408e932
Vladislav <vladislavprokhorov1005@gmail.com>
2025-07-23 21:21:35 +0300

    Header

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+  const swiper = new Swiper('.swiper', {
+    loop: true,
+    slidesPerView: 3,
+    speed: 6000,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    freeMode: true,
+    grabCursor: false,
+  });
+
